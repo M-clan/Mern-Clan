05b93858968a8a152ae7b5d2a28b2f92753c79f9
vimal <vimalonboar69@gmail.com>
2022-12-11 23:53:49 +0530

    reducer Error

--- Login System using Redux/src/features/userSlice.js
+++ Login System using Redux/src/features/userSlice.js
@@ -4,19 +4,19 @@ export const userSlice =createSlice(
     {
       name:"user",
       initialState:{
-        user:null
+        user:null, 
       },
       reducers:{                              //reducer is gonna act as a function==> going to update our state or update redux
-        login:(state,action)=>{
-            state.user = action.payload        //user will be updated by the payload that is being passed
+        login: (state,action)=>{
+            state.user = action.payload  ;      //user will be updated by the payload that is being passed
         },
-        logic:(state)=>{ // here we dont need any payload : means we doesnt have anything to update (name , email ...etc)
+        logout: (state)=>{ // here we dont need any payload : means we doesnt have anything to update (name , email ...etc)
             state.user = null;
 
-        }
-      }
+        },
+      },
     } 
-)
+);
  export const {login,logout} = userSlice.actions;   //these are the acction
  export  const selectUser = (state) => state.user.user;   //grab the data from redux using selectUser 
 //here "state.user" is state and again another  ".user"is for updating it with more information ,so defined in another obect ".user" 
