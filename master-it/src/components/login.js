261438f2d5fa6c19a96e56f671248cd41f53bff9
vimal <vimalonboar69@gmail.com>
2022-12-11 17:45:41 +0530

    file added

---
+++ master-it/src/components/login.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function login() {
+  return (
+    <div className='login'>
+        <form className='login-form'>
+         <h1>Login Here </h1>   
+
+        </form>
+
+    </div>
+  )
+}
+
+export default login
